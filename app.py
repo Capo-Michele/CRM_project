ba382a35f1c442566d67ee89866865ed47648a23
Mikhail <micaaromic@gmail.com>
2026-05-03 21:50:32 +0300

    post added

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template ('index.html')
+    user = {'username': "Elena", 'age': 28}
+
+    posts = [
+        {'author': "Vanya", 'body': 'This is my first post!'},
+        {'author': "Mary", 'body': 'Flask is cool!'}
+    ]
+    return render_template ('index.html', posts = posts, user=user, title="Main")
 
 @app.route('/user/<username>')
-def user_nane(username):
+def user_name(username):
     return f'<h1>user\'s name: {username}</h1>'
 
 if __name__ == '__main__':
