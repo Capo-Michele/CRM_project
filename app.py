b975faca95940257b71a7f556159a0366b37033c
Mikhail <micaaromic@gmail.com>
2026-05-07 11:49:42 +0300

    Добавил логирование и чуть css

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.config ['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
-    user = {'username': "Elena", 'age': 28}
+    user = {'username': "Васёк", 'age': 28}
 
     posts = [
-        {'author': "Vanya", 'body': 'This is my first post!'},
+        {'author': "notVanya", 'body': 'This is my first post!'},
         {'author': "Mary", 'body': 'Flask is cool!'}
     ]
-    return render_template ('index.html', posts = posts, user=user, title="Main")
+    return render_template ('index.html', posts = posts, user=user, title="This is my site")
 
 @app.route('/user/<username>')
 def user_name(username):
-    return f'<h1>user\'s name: {username}</h1>'
+    return (f'<h1>user\'s name: {username}</h1>')
 
 if __name__ == '__main__':
     app.run(debug=True)
