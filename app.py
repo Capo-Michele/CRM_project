8a946c62c2c1daf762b4ebdf69e802344768f6bc
Mikhail <micaaromic@gmail.com>
2026-05-11 16:50:03 +0300

    04.05.2026

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import (Flask, render_template, request, flash, redirect, url_for)
+
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306//flaskapp'
+
+
 app.config ['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
@@ -15,6 +22,20 @@ def index():
 @app.route('/user/<username>')
 def user_name(username):
     return (f'<h1>user\'s name: {username}</h1>')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username == "admin" and password == 'admin':
+            flash ("you've successfully entered", "success")
+            return redirect (url_for ('index'))
+        else: 
+            flash ("you've failed", "error")
+            return redirect (url_for ('login'))
+
+    return render_template ('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
